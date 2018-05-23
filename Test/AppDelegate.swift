c5d60ed18d39253d54da55fd3517821a539527ce
apple <ligongbo@huihaiedu.net>
2018-05-23 09:59:30 +0800

    no message

--- Test/AppDelegate.swift
+++ Test/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        //aaaa
         let session = AVAudioSession.sharedInstance()
         do{
             try session.setCategory(AVAudioSessionCategoryPlayback)
